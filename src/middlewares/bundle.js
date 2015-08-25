936bd708b0fe87d75b43fc86d316f8846f1a4dc2
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2015-08-25 15:11:08 -0400

    Fix data parameter bug while paginating

--- src/middlewares/bundle.js
+++ src/middlewares/bundle.js
@@ -5,6 +5,7 @@ exports.$$BundleLinkUrl =  function(rel){
             var res =  args.bundle && (args.bundle.link || []).filter(matched)[0];
             if(res && res.url){
                 args.url = res.url;
+                args.data = null;
                 return h(args);
             }
             else{
