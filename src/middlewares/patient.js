c7592fc0934c132ffcf794da3b2a6c529af85848
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2015-09-03 11:41:36 -0400

    Add mechanism for selective application of patient context

--- src/middlewares/patient.js
+++ src/middlewares/patient.js
@@ -1,18 +1,23 @@
 (function() {
     var mw = require('./core');
+    
+    var keyFor = [
+        "Observation",
+        "MedicationPrescription",
+        "Encounter"
+        // TO DO: add remaining resources with Patient here
+    ];
 
     exports.$WithPatient = mw.$$Simple(function(args){
+        var type = args.type;
         if (args.patient) {
-            if (args.type === "Patient") {
+            if (type === "Patient") {
                 args.query = args.query || {};
                 args.query["_id"] = args.patient;
                 args["id"] = args.patient;
-            } else {
+            } else if (keyFor.indexOf(type) >= 0){
                 args.query = args.query || {};
-                args.query["patient"] = {
-                    $type: "Patient",
-                    _id: args.patient
-                };
+                args.query["patient"] = args.patient;
             }
         }
         return args;
