23837507d409ab8ab82b19f2ba01d64409ff6ffd
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2015-09-03 16:41:02 -0400

    Update list of resources with patient or subject properties

--- src/middlewares/patient.js
+++ src/middlewares/patient.js
@@ -1,11 +1,56 @@
 (function() {
     var mw = require('./core');
-    
-    var keyFor = [
+
+    // List of resources with 'patient' or 'subject' properties (as of FHIR DSTU2 1.0.0)
+    var targets = [
+        "Account",
+        "AllergyIntolerance",
+        "BodySite",
+        "CarePlan",
+        "Claim",
+        "ClinicalImpression",
+        "Communication",
+        "CommunicationRequest",
+        "Composition",
+        "Condition",
+        "Contract",
+        "DetectedIssue",
+        "Device",
+        "DeviceUseRequest",
+        "DeviceUseStatement",
+        "DiagnosticOrder",
+        "DiagnosticReport",
+        "DocumentManifest",
+        "DocumentReference",
+        "Encounter",
+        "EnrollmentRequest",
+        "EpisodeOfCare",
+        "FamilyMemberHistory",
+        "Flag",
+        "Goal",
+        "ImagingObjectSelection",
+        "ImagingStudy",
+        "Immunization",
+        "ImmunizationRecommendation",
+        "List",
+        "Media",
+        "MedicationAdministration",
+        "MedicationDispense",
+        "MedicationOrder",
+        "MedicationStatement",
+        "NutritionOrder",
         "Observation",
-        "MedicationPrescription",
-        "Encounter"
-        // TO DO: add remaining resources with Patient here
+        "Order",
+        "Procedure",
+        "ProcedureRequest",
+        "QuestionnaireResponse",
+        "ReferralRequest",
+        "RelatedPerson",
+        "RiskAssessment",
+        "Specimen",
+        "SupplyDelivery",
+        "SupplyRequest",
+        "VisionPrescription"
     ];
 
     exports.$WithPatient = mw.$$Simple(function(args){
@@ -15,7 +60,7 @@
                 args.query = args.query || {};
                 args.query["_id"] = args.patient;
                 args["id"] = args.patient;
-            } else if (keyFor.indexOf(type) >= 0){
+            } else if (targets.indexOf(type) >= 0){
                 args.query = args.query || {};
                 args.query["patient"] = args.patient;
             }
