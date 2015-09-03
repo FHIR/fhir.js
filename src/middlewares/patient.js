e62ded300bb54711489eb7eba9028e18aef14481
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2015-09-03 08:35:04 -0400

    All resources in DSTU2 have patient property or alias

--- src/middlewares/patient.js
+++ src/middlewares/patient.js
@@ -1,27 +1,18 @@
 (function() {
     var mw = require('./core');
 
-    var keyFor = {
-        "Observation": "subject",
-        "MedicationPrescription": "patient",
-        "Encounter": "patient"
-        // TO DO: add remaining resource mappings here
-    };
-
     exports.$WithPatient = mw.$$Simple(function(args){
-        var type  = args.type;
-        var param = keyFor[type];
         if (args.patient) {
-            if (param){
-                args.query = args.query || {};
-                args.query[param] = {
-                    $type: "Patient",
-                    _id: args.patient
-                };
-            } else if (type === "Patient") {
+            if (args.type === "Patient") {
                 args.query = args.query || {};
                 args.query["_id"] = args.patient;
                 args["id"] = args.patient;
+            } else {
+                args.query = args.query || {};
+                args.query["patient"] = {
+                    $type: "Patient",
+                    _id: args.patient
+                };
             }
         }
         return args;
